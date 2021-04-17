3e97cae06e25340fb5048c19675dc0542d04c1f6
arogan178 <andrea.bugeja@hotmail.com>
2021-04-17 18:51:32 +0200

    Splash Screen Added

--- js/toggle.js
+++ js/toggle.js
@@ -2,37 +2,37 @@ const toggle = document.getElementById('toggle-input');
 const lightIcon = document.getElementById('light-icon');
 const darkIcon = document.getElementById('dark-icon');
 
-function setTheme(theme, persist = false) {
-    const on = theme;
-    const off = theme === 'light' ? 'dark' : 'light'
+function setTheme (theme, persist = false) {
+  const on = theme;
+  const off = theme === 'light' ? 'dark' : 'light';
 
-    const htmlEl = document.documentElement;
-    htmlEl.classList.add(on);
-    htmlEl.classList.remove(off);
+  const htmlEl = document.documentElement;
+  htmlEl.classList.add(on);
+  htmlEl.classList.remove(off);
 
-    if (persist) {
-        localStorage.setItem('preferred-theme', theme);
-    }
+  if (persist) {
+   localStorage.setItem('preferred-theme', theme);
+  }
 }
 
-function updateUI(theme) {
-    toggle.checked = theme === 'light';
+function updateUI (theme) {
+  toggle.checked = theme === 'light';
 
-    /* Show the right icon */
-    if (theme === 'light') {
-        lightIcon.classList.add('active');
-        darkIcon.classList.remove('active');
-    } else {
-        darkIcon.classList.add('active');
-        lightIcon.classList.remove('active');
-    }
+  /* Show the right icon */
+  if (theme === 'light') {
+    lightIcon.classList.add('active');
+    darkIcon.classList.remove('active');
+  } else {
+    darkIcon.classList.add('active');
+    lightIcon.classList.remove('active');
+  }
 }
 
 toggle.addEventListener('click', () => {
-    const theme = toggle.checked ? 'light' : 'dark';
-    setTheme(theme, true);
-    updateUI(theme);
-});
+  const theme = toggle.checked ? 'light' : 'dark';
+  setTheme(theme, true);
+  updateUI(theme);
+})
 
 /* Set theme on load */
 const osPreference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
