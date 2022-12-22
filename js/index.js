ccf3c96ad47d915123b92cb9fc775a425617cf96
arogan178 <andrea.bugeja@hotmail.com>
2022-12-22 23:50:00 +0100

    splash screen bug fix + experience content added

--- js/index.js
+++ js/index.js
@@ -2,11 +2,19 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
-  document.body.classList.toggle('nav-open');
+    document.body.classList.toggle('nav-open');
 });
 
-navLinks.forEach(link  => {
-  link.addEventListener('click', () => {
-    document.body.classList.remove('nav-open');
-  })
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
 });
+
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function() {
+        window.scrollTo(0, 0);
+    }
+}
