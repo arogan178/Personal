b83310cc6b3573970d6f5585c88ee195fd046268
arogan178 <andrea.bugeja@hotmail.com>
2023-02-10 00:01:35 +0100

    - Further polishing - Added hover + underline effect to name - Fixed resize bug when opening nav menu - Remove code that is no longer used (splash screen) + updated JS file - This commit is to be pushed to Dev and later to Prod

--- js/splash.js
+++
@@ -1,7 +0,0 @@
-var element = document.getElementById("splashImage");
-element.addEventListener("mousewheel", MouseWheelHandler, false);
-element.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
-function MouseWheelHandler(e) {
-      document.getElementById("clickScroll").checked = true;
-      return false;
-}
