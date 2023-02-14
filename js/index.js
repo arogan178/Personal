01cb4b0f87b2f74a76c565ad6d62bd503ff043b7
arogan178 <andrea.bugeja@hotmail.com>
2023-02-14 13:58:44 +0100

    + Fixed div for Theme selector + Fixed clickable invisible btns on page load + Cleaned up code + Refactored index.js

--- js/index.js
+++ js/index.js
@@ -1,48 +1,69 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+function navToggle() {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelectorAll('.nav__link')
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+    navToggle.addEventListener('click', () => {
+        document.body.classList.toggle('nav-open');
+        if (document.body.classList.contains('nav-open')) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-});
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            document.body.classList.remove('nav-open');
+            document.body.style.overflow = 'auto';
+        })
+    });
+}
 
-if (history.scrollRestoration) {
-    history.scrollRestoration = 'manual';
-} else {
-    window.onbeforeunload = function () {
-        window.scrollTo(0, 0);
+function headerBarOpacity() {
+    const headerBar = document.querySelector('.header__bar');
+    const themeSelector = document.querySelector('.theme__selector');
+    const navButton = document.querySelector('.nav-toggle');
+
+    //Vary opacity of header bar based on scroll position
+    let scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    headerBar.style.opacity = Math.min(1, 0 + (scrollPos / 500));
+
+    //if opactiy <70 disable functionality
+    if (headerBar.style.opacity <= 0.7) {
+        themeSelector.style.pointerEvents = 'none';
+        navButton.style.pointerEvents = 'none';
+    } else {
+        themeSelector.style.pointerEvents = 'auto';
+        navButton.style.pointerEvents = 'auto';
     }
 }
 
-window.onscroll = function () {
-    const headerBar = document.querySelector(".header__bar");
-    const themeSelector = document.querySelector(".theme__selector");
-    const navButton = document.querySelector(".nav__button");
+function sectionVisibility() {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio >= 0.8) {
+                entry.target.classList.add("is-visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
     const aboutMe = document.querySelector(".about-me");
+    const education = document.querySelector(".my-education");
     const myExperience = document.querySelector(".my-experience");
+    
 
-    let scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    ;
-    headerBar.style.opacity = (0 + scrollPos / 500);
+    observer.observe(aboutMe);
+    observer.observe(education);
+    observer.observe(myExperience);
+}
 
-    if (scrollPos >= 300) {
-        aboutMe.classList.add("is-visible");
-    }
-    if (scrollPos >= 1000) {
-        myExperience.classList.add("is-visible");
-    }
+window.onscroll = function () {
+    headerBarOpacity();
+    sectionVisibility();
+};
 
-    if (headerBar.style.opacity > 0 && headerBar.style.opacity < 0.7) {
-        themeSelector.style.pointerEvents = "none";
-        navButton.style.pointerEvents = "none";
-    } else {
-        themeSelector.style.pointerEvents = "auto";
-        navButton.style.pointerEvents = "auto";
-    }
-
+window.onload = function () {
+    headerBarOpacity();
+    navToggle();
 };
